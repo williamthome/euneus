64532d67ae20fc2a52eca64c7afe58d1504d1f87
William Fank Thomé <willegp88@gmail.com>
2023-10-24 01:16:30 -0300

    chore: add copyright

--- src/euneus.erl
+++ src/euneus.erl
@@ -1,3 +1,21 @@
+%% @author William Fank Thomé <willilamthome@hotmail.com>
+%% @copyright 2023 William Fank Thomé
+%% @title Euneus
+%% @doc Core module to parse and generate JSON.
+
+%% Copyright 2023 William Fank Thomé
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
 -module(euneus).
 
 -compile({inline, [ encode/1, encode_to_binary/1, encode_to_binary/2, decode/1 ]}).
