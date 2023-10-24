635df30ae2f860407e2369d2acf020cc4a4ce12f
William Fank Thomé <willegp88@gmail.com>
2023-10-24 01:09:52 -0300

    perf(encoder): improve performance

--- src/euneus_encoder.erl
+++ src/euneus_encoder.erl
@@ -1,28 +1,38 @@
 -module(euneus_encoder).
 
 -compile({inline, [
-    do_encode/2, encode_binary/2, encode_atom/2, encode_integer/2,
-    encode_float/2, encode_list/2, encode_map/2, encode_datetime/2,
-    encode_timestamp/2, encode_unhandled/2, do_encode_list_loop/2,
-    do_encode_map/2, do_encode_map_loop/2, escape_binary/2,
-    escape_json/4, escape_json_chunk/5, escape_byte/1
+    % misc
+    parse_opts/1,
+    % encode
+    encode_binary/2, encode_atom/2, encode_integer/2, encode_float/2,
+    encode_list/2, encode_map/2, encode_datetime/2, encode_timestamp/2,
+    encode_unhandled/2,
+    % escape
+    escape_json/1
 ]}).
 -compile({inline_size, 100}).
 
 -export([ encode/2 ]).
--export([ escape_byte/1 ]).
+-export([
+    encode_binary/2, encode_atom/2, encode_integer/2, encode_float/2,
+    encode_list/2, encode_map/2, encode_datetime/2, encode_timestamp/2,
+    encode_unhandled/2
+]).
+-export([ unsupported_type_error/2 ]).
+-export([ escape_binary/2, escape_byte/1 ]).
 -export([ escape_json/1, escape_html/1, escape_js/1, escape_unicode/1 ]).
 
--define(int_min(X, Min), is_integer(X) andalso X >= Min).
--define(int_range(X, Min, Max), is_integer(X) andalso X >= Min andalso X =< Max).
+-define(min(X, Min), is_integer(X) andalso X >= Min).
+-define(range(X, Min, Max), is_integer(X) andalso X >= Min andalso X =< Max).
 
 encode(Term, Opts) ->
-    do_encode(Term, parse_opts(Opts)).
+    value(Term, parse_opts(Opts)).
 
 % The explicit call to encode_*/2 wrapped in a function is required
 % for the inline optimization.
 parse_opts(Opts) ->
     #{
+        null_terms => maps:get(null_terms, Opts, [undefined]),
         encode_binary => maps:get(encode_binary, Opts, fun (X, O) ->
             encode_binary(X, O)
         end),
@@ -51,47 +61,53 @@ parse_opts(Opts) ->
             encode_unhandled(X, O)
         end),
         escape_binary => maps:get(escape_binary, Opts, fun (X, _O) ->
-            escape_json(X, [], X, 0)
+            [$", escape_json(X), $"]
         end)
     }.
 
-do_encode(Bin, #{encode_binary := Encode} = Opts) when is_binary(Bin) ->
+key(Bin, #{encode_binary := Encode} = Opts) when is_binary(Bin) ->
     Encode(Bin, Opts);
-do_encode(Atom, #{encode_atom := Encode} = Opts) when is_atom(Atom) ->
+key(Atom, #{encode_atom := Encode} = Opts) when is_atom(Atom) ->
     Encode(Atom, Opts);
-do_encode(Int, #{encode_integer := Encode} = Opts) when is_integer(Int) ->
+key(Int, #{encode_integer := Encode} = Opts) when is_integer(Int) ->
+    Encode(Int, Opts).
+
+value(Bin, #{encode_binary := Encode} = Opts) when is_binary(Bin) ->
+    Encode(Bin, Opts);
+value(Atom, #{encode_atom := Encode} = Opts) when is_atom(Atom) ->
+    Encode(Atom, Opts);
+value(Int, #{encode_integer := Encode} = Opts) when is_integer(Int) ->
     Encode(Int, Opts);
-do_encode(Float, #{encode_float := Encode} = Opts) when is_float(Float) ->
+value(Float, #{encode_float := Encode} = Opts) when is_float(Float) ->
     Encode(Float, Opts);
-do_encode(List, #{encode_list := Encode} = Opts) when is_list(List) ->
+value(List, #{encode_list := Encode} = Opts) when is_list(List) ->
     Encode(List, Opts);
-do_encode(Map, #{encode_map := Encode} = Opts) when is_map(Map) ->
+value(Map, #{encode_map := Encode} = Opts) when is_map(Map) ->
     Encode(Map, Opts);
-do_encode({{YYYY,MM,DD},{H,M,S}} = DateTime, #{encode_datetime := Encode} = Opts)
-  when ?int_min(YYYY, 0), ?int_range(MM, 1, 12), ?int_range(DD, 1, 31)
-     , ?int_range(H, 0, 23), ?int_range(M, 0, 59), ?int_range(S, 0, 59) ->
+value({{YYYY,MM,DD},{H,M,S}} = DateTime, #{encode_datetime := Encode} = Opts)
+  when ?min(YYYY, 0), ?range(MM, 1, 12), ?range(DD, 1, 31)
+     , ?range(H, 0, 23), ?range(M, 0, 59), ?range(S, 0, 59) ->
     Encode(DateTime, Opts);
-do_encode({MegaSecs,Secs,MicroSecs} = Timestamp, #{encode_timestamp := Encode} = Opts)
-  when ?int_min(MegaSecs, 0), ?int_min(Secs, 0), ?int_min(MicroSecs, 0) ->
+value({MegaSecs,Secs,MicroSecs} = Timestamp, #{encode_timestamp := Encode} = Opts)
+  when ?min(MegaSecs, 0), ?min(Secs, 0), ?min(MicroSecs, 0) ->
     Encode(Timestamp, Opts);
-do_encode(Term, #{encode_unhandled := Encode} = Opts) ->
+value(Term, #{encode_unhandled := Encode} = Opts) ->
     Encode(Term, Opts).
 
 encode_binary(Bin, Opts) ->
-    [$", escape_binary(Bin, Opts), $"].
+    escape_binary(Bin, Opts).
 
 encode_atom(true, _Opts) ->
     <<"true">>;
 encode_atom(false, _Opts) ->
     <<"false">>;
-encode_atom(undefined, _Opts) ->
-    <<"null">>;
-encode_atom(nil, _Opts) ->
-    <<"null">>;
-encode_atom(null, _Opts) ->
-    <<"null">>;
-encode_atom(Atom, Opts) ->
-    [$", escape_binary(atom_to_binary(Atom, utf8), Opts), $"].
+encode_atom(Atom, #{null_terms := Nulls} = Opts) ->
+    case lists:member(Atom, Nulls) of
+        true ->
+            <<"null">>;
+        false ->
+            escape_binary(atom_to_binary(Atom, utf8), Opts)
+    end.
 
 encode_integer(Int, _Opts) ->
     integer_to_binary(Int).
@@ -99,35 +115,35 @@ encode_integer(Int, _Opts) ->
 encode_float(Float, _Opts) ->
     float_to_binary(Float, [short]).
 
-encode_list([First | Rest], Opts) ->
-    [$[, do_encode(First, Opts) | do_encode_list_loop(Rest, Opts)];
+encode_list([H | T], Opts) ->
+    [$[, value(H, Opts) | do_encode_list_loop(T, Opts)];
 encode_list([], _Opts) ->
     <<"[]">>.
 
+do_encode_list_loop([H | T], Opts) ->
+    [$,, value(H, Opts) | do_encode_list_loop(T, Opts)];
 do_encode_list_loop([], _Opts) ->
-    [$]];
-do_encode_list_loop([First | Rest], Opts) ->
-    [$,, do_encode(First, Opts) | do_encode_list_loop(Rest, Opts)].
+    [$]].
 
 encode_map(Map, Opts) ->
     do_encode_map(maps:to_list(Map), Opts).
 
 do_encode_map([{K, V} | T], Opts) ->
-    [${, do_encode(K, Opts), $:, do_encode(V, Opts) | do_encode_map_loop(T, Opts)];
+    [${, key(K, Opts), $:, value(V, Opts) | do_encode_map_loop(T, Opts)];
 do_encode_map([], _) ->
     <<"{}">>.
 
-do_encode_map_loop([], _Opts) ->
-    [$}];
 do_encode_map_loop([{K, V} | T], Opts) ->
-    [$,, do_encode(K, Opts), $:, do_encode(V, Opts) | do_encode_map_loop(T, Opts)].
+    [$,, key(K, Opts), $:, value(V, Opts) | do_encode_map_loop(T, Opts)];
+do_encode_map_loop([], _Opts) ->
+    [$}].
 
 encode_datetime({{YYYY,MM,DD},{H,M,S}}, Opts) ->
     DateTime = iolist_to_binary(io_lib:format(
         "~4.10.0B-~2.10.0B-~2.10.0BT~2.10.0B:~2.10.0B:~2.10.0BZ",
         [YYYY,MM,DD,H,M,S])
     ),
-    [$", escape_binary(DateTime, Opts), $"].
+    escape_binary(DateTime, Opts).
 
 encode_timestamp({_,_,MicroSecs} = Timestamp, Opts) ->
     MilliSecs = MicroSecs div 1000,
@@ -136,19 +152,56 @@ encode_timestamp({_,_,MicroSecs} = Timestamp, Opts) ->
         "~4.10.0B-~2.10.0B-~2.10.0BT~2.10.0B:~2.10.0B:~2.10.0B.~3.10.0BZ",
         [YYYY,MM,DD,H,M,S,MilliSecs])
     ),
-    [$", escape_binary(DateTime, Opts), $"].
+    escape_binary(DateTime, Opts).
 
 encode_unhandled(Term, Opts) ->
-    error(unsupported_type, [Term, Opts]).
+    unsupported_type_error(Term, Opts).
 
 escape_binary(Bin, #{escape_binary := Escape} = Opts) ->
     Escape(Bin, Opts).
+
+escape_byte(0) -> <<"\\u0000">>;
+escape_byte(1) -> <<"\\u0001">>;
+escape_byte(2) -> <<"\\u0002">>;
+escape_byte(3) -> <<"\\u0003">>;
+escape_byte(4) -> <<"\\u0004">>;
+escape_byte(5) -> <<"\\u0005">>;
+escape_byte(6) -> <<"\\u0006">>;
+escape_byte(7) -> <<"\\u0007">>;
+escape_byte(8) -> <<"\\b">>;
+escape_byte(9) -> <<"\\t">>;
+escape_byte(10) -> <<"\\n">>;
+escape_byte(11) -> <<"\\u000B">>;
+escape_byte(12) -> <<"\\f">>;
+escape_byte(13) -> <<"\\r">>;
+escape_byte(14) -> <<"\\u000E">>;
+escape_byte(15) -> <<"\\u000F">>;
+escape_byte(16) -> <<"\\u0010">>;
+escape_byte(17) -> <<"\\u0011">>;
+escape_byte(18) -> <<"\\u0012">>;
+escape_byte(19) -> <<"\\u0013">>;
+escape_byte(20) -> <<"\\u0014">>;
+escape_byte(21) -> <<"\\u0015">>;
+escape_byte(22) -> <<"\\u0016">>;
+escape_byte(23) -> <<"\\u0017">>;
+escape_byte(24) -> <<"\\u0018">>;
+escape_byte(25) -> <<"\\u0019">>;
+escape_byte(26) -> <<"\\u001A">>;
+escape_byte(27) -> <<"\\u001B">>;
+escape_byte(28) -> <<"\\u001C">>;
+escape_byte(29) -> <<"\\u001D">>;
+escape_byte(30) -> <<"\\u001E">>;
+escape_byte(31) -> <<"\\u001F">>;
+escape_byte(34) -> <<"\\\"">>;
+escape_byte(47) -> <<"\\/">>;
+escape_byte(92) -> <<"\\\\">>;
+escape_byte(Byte) -> invalid_byte_error(Byte, Byte).
 
 escape_json(Bin) ->
     escape_json(Bin, [], Bin, 0).
 
 escape_json(<<Byte/integer, Rest/bitstring>>, Acc0, Input, Skip)
-    when Byte < 32; Byte =:= 34; Byte =:= 92 ->
+    when Byte < 32; Byte =:= $\"; Byte =:= $\\ ->
     Acc = [Acc0 | escape_byte(Byte)],
     escape_json(Rest, Acc, Input, Skip+1);
 escape_json(<<Byte/integer, Rest/bitstring>>, Acc, Input, Skip)
@@ -168,7 +221,7 @@ escape_json(<<Byte/integer, _Rest/bitstring>>, _Acc, Input, _Skip) ->
     invalid_byte_error(Byte, Input).
 
 escape_json_chunk(<<Byte/integer, Rest/bitstring>>, Acc0, Input, Skip, Len)
-    when Byte < 32; Byte =:= 34; Byte =:= 92 ->
+    when Byte < 32; Byte =:= $\"; Byte =:= $\\ ->
     Part = binary_part(Input, Skip, Len),
     Acc = [Acc0, Part | escape_byte(Byte)],
     escape_json(Rest, Acc, Input, Skip+Len+1);
@@ -195,7 +248,7 @@ escape_html(Bin) ->
     escape_html(Bin, [], Bin, 0).
 
 escape_html(<<Byte/integer, Rest/bitstring>>, Acc0, Input, Skip)
-    when Byte < 33; Byte =:= 34; Byte =:= 47; Byte =:= 92 ->
+    when Byte < 33; Byte =:= $\"; Byte =:= $/; Byte =:= $\\ ->
     Acc = [Acc0 | escape_byte(Byte)],
     escape_html(Rest, Acc, Input, Skip+1);
 escape_html(<<Byte/integer, Rest/bitstring>>, Acc, Input, Skip)
@@ -221,7 +274,7 @@ escape_html(<<Byte/integer, _Rest/bitstring>>, _Acc, Input, _Skip) ->
     invalid_byte_error(Byte, Input).
 
 escape_html_chunk(<<Byte/integer, Rest/bitstring>>, Acc, Input, Skip, Len)
-    when Byte < 33; Byte =:= 34; Byte =:= 47; Byte =:= 92 ->
+    when Byte < 33; Byte =:= $\"; Byte =:= $/; Byte =:= $\\ ->
     Part = binary_part(Input, Skip, Len),
     Acc2 = [Acc, Part | escape_byte(Byte)],
     escape_html(Rest, Acc2, Input, Skip+Len+1);
@@ -256,7 +309,7 @@ escape_js(Bin) ->
     escape_js(Bin, [], Bin, 0).
 
 escape_js(<<Byte/integer, Rest/bitstring>>, Acc0, Input, Skip)
-    when Byte < 32; Byte =:= 34; Byte =:= 92 ->
+    when Byte < 32; Byte =:= $\"; Byte =:= $\\ ->
     Acc = [Acc0 | escape_byte(Byte)],
     escape_js(Rest, Acc, Input, Skip+1);
 escape_js(<<Byte/integer, Rest/bitstring>>, Acc, Input, Skip)
@@ -282,7 +335,7 @@ escape_js(<<Byte/integer, _Rest/bitstring>>, _Acc, Input, _Skip) ->
     invalid_byte_error(Byte, Input).
 
 escape_js_chunk(<<Byte/integer, Rest/bitstring>>, Acc0, Input, Skip, Len)
-    when Byte < 32; Byte =:= 34; Byte =:= 92 ->
+    when Byte < 32; Byte =:= $\"; Byte =:= $\\ ->
     Part = binary_part(Input, Skip, Len),
     Acc = [Acc0, Part | escape_byte(Byte)],
     escape_js(Rest, Acc, Input, Skip+Len+1);
@@ -317,7 +370,7 @@ escape_unicode(Bin) ->
     escape_unicode(Bin, [], Bin, 0).
 
 escape_unicode(<<Byte/integer, Rest/bitstring>>, Acc0, Input, Skip)
-  when Byte < 32; Byte =:= 34; Byte =:= 92 ->
+  when Byte < 32; Byte =:= $\"; Byte =:= $\\ ->
     Acc = [Acc0 | escape_byte(Byte)],
     escape_unicode(Rest, Acc, Input, Skip+1);
 escape_unicode(<<Byte/integer, Rest/bitstring>>, Acc, Input, Skip)
@@ -354,7 +407,7 @@ escape_unicode(<<Byte/integer, _Rest/bitstring>>, _Acc, Input, _Skip) ->
     invalid_byte_error(Byte, Input).
 
 escape_unicode_chunk(<<Byte/integer, Rest/bitstring>>, Acc0, Input, Skip, Len)
-  when Byte < 32; Byte =:= 34; Byte =:= 92 ->
+  when Byte < 32; Byte =:= $\"; Byte =:= $\\ ->
     Part = binary_part(Input, Skip, Len),
     Acc = [Acc0, Part | escape_byte(Byte)],
     escape_unicode(Rest, Acc, Input, Skip+Len+1);
@@ -400,42 +453,8 @@ escape_unicode_chunk(<<>>, Acc, Input, Skip, Len) ->
 escape_unicode_chunk(<<Byte/integer, _Rest/bitstring>>, _Acc, Input, _Skip, _Len) ->
     invalid_byte_error(Byte, Input).
 
-escape_byte(0) -> <<"\\u0000">>;
-escape_byte(1) -> <<"\\u0001">>;
-escape_byte(2) -> <<"\\u0002">>;
-escape_byte(3) -> <<"\\u0003">>;
-escape_byte(4) -> <<"\\u0004">>;
-escape_byte(5) -> <<"\\u0005">>;
-escape_byte(6) -> <<"\\u0006">>;
-escape_byte(7) -> <<"\\u0007">>;
-escape_byte(8) -> <<"\\b">>;
-escape_byte(9) -> <<"\\t">>;
-escape_byte(10) -> <<"\\n">>;
-escape_byte(11) -> <<"\\u000B">>;
-escape_byte(12) -> <<"\\f">>;
-escape_byte(13) -> <<"\\r">>;
-escape_byte(14) -> <<"\\u000E">>;
-escape_byte(15) -> <<"\\u000F">>;
-escape_byte(16) -> <<"\\u0010">>;
-escape_byte(17) -> <<"\\u0011">>;
-escape_byte(18) -> <<"\\u0012">>;
-escape_byte(19) -> <<"\\u0013">>;
-escape_byte(20) -> <<"\\u0014">>;
-escape_byte(21) -> <<"\\u0015">>;
-escape_byte(22) -> <<"\\u0016">>;
-escape_byte(23) -> <<"\\u0017">>;
-escape_byte(24) -> <<"\\u0018">>;
-escape_byte(25) -> <<"\\u0019">>;
-escape_byte(26) -> <<"\\u001A">>;
-escape_byte(27) -> <<"\\u001B">>;
-escape_byte(28) -> <<"\\u001C">>;
-escape_byte(29) -> <<"\\u001D">>;
-escape_byte(30) -> <<"\\u001E">>;
-escape_byte(31) -> <<"\\u001F">>;
-escape_byte(34) -> <<"\\\"">>;
-escape_byte(47) -> <<"\\/">>;
-escape_byte(92) -> <<"\\\\">>;
-escape_byte(Byte) -> invalid_byte_error(Byte, Byte).
+unsupported_type_error(Term, Opts) ->
+    error(unsupported_type, [Term, Opts]).
 
 invalid_byte_error(Byte0, Input) ->
     Byte = <<"0x"/utf8, (integer_to_binary(Byte0, 16))/binary>>,
@@ -453,7 +472,7 @@ encode_test() ->
         {<<"\"foo\"">>, <<"foo">>, #{}},
         {<<"0">>, 0, #{}},
         {<<"123.456789">>, 123.45678900, #{}},
-        {<<"[true,0]">>, [true, 0], #{}},
+        {<<"[true,0,null]">>, [true, 0, undefined], #{}},
         {<<"{\"foo\":\"bar\"}">>, #{foo => bar}, #{}},
         {<<"\"1970-01-01T00:00:00Z\"">>, {{1970,1,1},{0,0,0}}, #{}},
         {<<"\"1970-01-01T00:00:00.000Z\"">>, {0,0,0}, #{}}
