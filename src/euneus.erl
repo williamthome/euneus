24da35629cad583481d07db4e5b40c2983cc9185
William Fank Thomé <willegp88@gmail.com>
2024-07-28 15:03:48 -0300

    Create encode functions in euneus module

--- src/euneus.erl
+++ src/euneus.erl
@@ -4,12 +4,22 @@
 %% API function exports
 %% --------------------------------------------------------------------
 
+-export([encode/1]).
+-export([encode/2]).
 -export([decode/1]).
 -export([decode/2]).
 
 %% --------------------------------------------------------------------
 %% API functions
 %% --------------------------------------------------------------------
+
+-spec encode(term()) -> iodata().
+encode(Term) ->
+    euneus_encoder:encode(Term, #{}).
+
+-spec encode(term(), euneus_encoder:options()) -> iodata().
+encode(Term, Opts) ->
+    euneus_encoder:encode(Term, Opts).
 
 -spec decode(binary()) -> term().
 decode(JSON) ->
