b75af0ef6b1198269ad5aa25ca6e0660491d5d31
William Fank Thomé <willegp88@gmail.com>
2024-08-02 00:29:18 -0300

    Test encode benchmark files

--- test/euneus_benchmarker_SUITE.erl
+++ test/euneus_benchmarker_SUITE.erl
@@ -19,43 +19,63 @@ all() ->
 %% --------------------------------------------------------------------
 
 jiffy_decode_test(Config) when is_list(Config) ->
-    foreach_data_dir_file(
-        fun(JSON) ->
-            [
-                ?assertEqual(
-                    euneus:decode(JSON, #{}),
-                    jiffy:decode(JSON, [return_maps])
-                )
-            ]
+    map_data_dir_files(
+        fun({_Filename, JSON}) ->
+            ?assertEqual(
+                euneus:decode(JSON, #{}),
+                jiffy:decode(JSON, [return_maps])
+            )
         end,
         get_data_dir(Config)
     ).
 
 thoas_decode_test(Config) when is_list(Config) ->
-    foreach_data_dir_file(
-        fun(JSON) ->
-            [
-                ?assertEqual(
-                    euneus:decode(JSON, #{}),
-                    element(2, thoas:decode(JSON, #{}))
-                )
-            ]
+    map_data_dir_files(
+        fun({_Filename, JSON}) ->
+            ?assertEqual(
+                euneus:decode(JSON, #{}),
+                element(2, thoas:decode(JSON, #{}))
+            )
         end,
         get_data_dir(Config)
+    ).
+
+% We cannot test for equality here since keys of other libs are not sortable.
+encode_test(Config) when is_list(Config) ->
+    Data = read_decoded_data_dir_files(get_data_dir(Config)),
+    lists:foreach(
+        fun({_Filename, Term}) ->
+            euneus:encode(Term, #{}),
+            ?assert(true)
+        end,
+        Data
     ).
 
 %% --------------------------------------------------------------------
 %% Test support
 %% --------------------------------------------------------------------
 
+read_decoded_data_dir_files(DataDir) ->
+    map_data_dir_files(
+        fun({Filename, JSON}) ->
+            {Filename, euneus:decode(JSON)}
+        end,
+        DataDir
+    ).
+
 get_data_dir(Config) ->
     proplists:get_value(data_dir, Config).
 
-foreach_data_dir_file(Callback, DataDir) ->
+map_data_dir_files(Callback, DataDir) ->
     {ok, Files} = file:list_dir(DataDir),
-    [Callback(read_data_dir_file(DataDir, Filename)) || Filename <- Files].
+    [
+        Callback({
+            filename:join(DataDir, Filename),
+            read_file(filename:join(DataDir, Filename))
+        })
+     || Filename <- Files
+    ].
 
-read_data_dir_file(DataDir, Filename) ->
-    AbsFilename = filename:join(DataDir, Filename),
-    {ok, Binary} = file:read_file(AbsFilename),
+read_file(Filename) ->
+    {ok, Binary} = file:read_file(Filename),
     Binary.
